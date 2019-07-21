5cee8a0438f52f145d108b362664883cf2e14323
Michał Czapkowicz <czapkowicz@nonstop.co>
2019-07-21 20:58:47 +0200

    Updates

---
+++ precache-manifest.921f4826642967745ea616fc8711aac4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7bcc680184c5854ebcb3aa374ac5ad9",
+    "url": "/travellers/index.html"
+  },
+  {
+    "revision": "fb6aa892549317100268",
+    "url": "/travellers/static/css/main.c2b6509d.chunk.css"
+  },
+  {
+    "revision": "5fdefc8a2943fc5714d9",
+    "url": "/travellers/static/js/2.5d9daba2.chunk.js"
+  },
+  {
+    "revision": "fb6aa892549317100268",
+    "url": "/travellers/static/js/main.75fe83f2.chunk.js"
+  },
+  {
+    "revision": "1b5831f674a7da02ef54",
+    "url": "/travellers/static/js/runtime~main.6f2d384a.js"
+  },
+  {
+    "revision": "e73c59a9c7ef24a7fb0f33b77594dfad",
+    "url": "/travellers/static/media/img1.e73c59a9.jpg"
+  },
+  {
+    "revision": "483edce8b008dc82065a628362638fbf",
+    "url": "/travellers/static/media/img2.483edce8.jpg"
+  },
+  {
+    "revision": "aa8e8a0fc530bbeee8ec86f260572af6",
+    "url": "/travellers/static/media/poster.aa8e8a0f.jpg"
+  },
+  {
+    "revision": "1e6bf2ab3948539a63734b08d67c8fb7",
+    "url": "/travellers/static/media/trip-wave.1e6bf2ab.jpg"
+  },
+  {
+    "revision": "7b1a48bf5a281b224a5a16b4ee8d52e6",
+    "url": "/travellers/static/media/video.7b1a48bf.mp4"
+  }
+]);
